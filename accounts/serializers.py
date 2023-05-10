24af50f522a4d206fffb26d3de24f066384bd452
Mustafa Kemal Buçkun <mkbuckun@gmail.com>
2023-05-10 20:44:58 +0300

    Fixed a bug related to invitations and fixed an issue about notifications

--- accounts/serializers.py
+++ accounts/serializers.py
@@ -13,8 +13,20 @@ class NotificationSerializer(serializers.ModelSerializer):
 
 class InvitationSerializer(serializers.ModelSerializer):
     inviter = serializers.CharField(source='inviter.username')
-    project = serializers.PrimaryKeyRelatedField(queryset=Project.objects.all())
+    project = serializers.SerializerMethodField()
 
     class Meta:
         model = Invitation
         fields = '__all__'
+
+    def get_project(self, invitation):
+        project = {
+            'id': invitation.project.id,
+            'name': invitation.project.name,
+        }
+        if invitation.project.company:
+            project['company'] = {
+                'id': invitation.project.company.id,
+                'name': invitation.project.company.name,
+            }
+        return project
